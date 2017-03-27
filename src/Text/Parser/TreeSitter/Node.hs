ad904349e331fe430926389a825cdffb7b444ac5
Rob Rix <rob.rix@me.com>
2017-03-27 15:47:53 -0400

    Bridge the batch-copying functions.

--- src/Text/Parser/TreeSitter/Node.hs
+++ src/Text/Parser/TreeSitter/Node.hs
@@ -47,17 +47,6 @@ instance Storable TSNode where
 
 
 foreign import ccall "src/bridge.c ts_document_root_node_p" ts_document_root_node_p :: Ptr Document -> Ptr Node -> IO ()
-foreign import ccall "src/bridge.c ts_node_p_name" ts_node_p_name :: Ptr Node -> Ptr Document -> IO CString
-foreign import ccall "src/bridge.c ts_node_p_child_count" ts_node_p_child_count :: Ptr Node -> IO CSize
-foreign import ccall "src/bridge.c ts_node_p_named_child_count" ts_node_p_named_child_count :: Ptr Node -> IO CSize
-foreign import ccall "src/bridge.c ts_node_p_child" ts_node_p_child :: Ptr Node -> CSize -> Ptr Node -> IO CSize
-foreign import ccall "src/bridge.c ts_node_p_named_child" ts_node_p_named_child :: Ptr Node -> CSize -> Ptr Node -> IO CSize
-foreign import ccall "src/bridge.c ts_node_p_start_char" ts_node_p_start_char :: Ptr Node -> CSize
-foreign import ccall "src/bridge.c ts_node_p_end_char" ts_node_p_end_char :: Ptr Node -> CSize
-foreign import ccall "src/bridge.c ts_node_p_start_byte" ts_node_p_start_byte :: Ptr Node -> CSize
-foreign import ccall "src/bridge.c ts_node_p_end_byte" ts_node_p_end_byte :: Ptr Node -> CSize
 
-foreign import ccall "src/bridge.c ts_node_p_start_point_row" ts_node_p_start_point_row :: Ptr Node -> CSize
-foreign import ccall "src/bridge.c ts_node_p_start_point_column" ts_node_p_start_point_column :: Ptr Node -> CSize
-foreign import ccall "src/bridge.c ts_node_p_end_point_row" ts_node_p_end_point_row :: Ptr Node -> CSize
-foreign import ccall "src/bridge.c ts_node_p_end_point_column" ts_node_p_end_point_column :: Ptr Node -> CSize
+foreign import ccall "src/bridge.c ts_node_copy_named_child_nodes" ts_node_copy_named_child_nodes :: Ptr Document -> Ptr Node -> Ptr Node -> CSize -> IO ()
+foreign import ccall "src/bridge.c ts_node_copy_child_nodes" ts_node_copy_child_nodes :: Ptr Document -> Ptr Node -> Ptr Node -> CSize -> IO ()
