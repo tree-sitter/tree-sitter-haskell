c187364ca308d9df4c29d08f7714d068849c985a
Rob Rix <rob.rix@me.com>
2017-03-27 21:09:31 -0400

    Implement peek/poke manually and sizeOf statically.

--- src/Text/Parser/TreeSitter/Node.hs
+++ src/Text/Parser/TreeSitter/Node.hs
@@ -42,22 +42,45 @@ pokeAdvance ptr a = do
 {-# INLINE pokeAdvance #-}
 
 instance Storable Node where
-  alignment = cAlignment
-  sizeOf = cSizeOf
-  peek = cPeek
+  alignment _ = alignment (TSNode nullPtr 0 0 0 :: TSNode)
+  sizeOf _ = 64
+  peek ptr = do
+    (nodeTSNode, ptr) <- peekAdvance (castPtr ptr)
+    (nodeType, ptr) <- peekAdvance ptr
+    (nodeStartPoint, ptr) <- peekAdvance ptr
+    (nodeEndPoint, ptr) <- peekAdvance ptr
+    (nodeStartByte, ptr) <- peekAdvance ptr
+    (nodeEndByte, ptr) <- peekAdvance ptr
+    (nodeNamedChildCount, ptr) <- peekAdvance ptr
+    (nodeChildCount, ptr) <- peekAdvance ptr
+    return $! Node nodeTSNode nodeType nodeStartPoint nodeEndPoint nodeStartByte nodeEndByte nodeNamedChildCount nodeChildCount
   poke = cPoke
 
 instance Storable Point where
-  alignment = cAlignment
-  sizeOf = cSizeOf
-  peek = cPeek
+  alignment _ = alignment (0 :: Int32)
+  sizeOf _ = 8
+  peek ptr = do
+    (pointRow, ptr) <- peekAdvance (castPtr ptr)
+    (pointColumn, ptr) <- peekAdvance ptr
+    return $! Point pointRow pointColumn
   poke = cPoke
 
 instance Storable TSNode where
-  alignment = cAlignment
-  sizeOf = cSizeOf
-  peek = cPeek
-  poke = cPoke
+  alignment _ = alignment (nullPtr :: Ptr ())
+  sizeOf _ = 24
+  peek ptr = do
+    (p, ptr) <- peekAdvance (castPtr ptr)
+    (o1, ptr) <- peekAdvance ptr
+    (o2, ptr) <- peekAdvance ptr
+    (o3, ptr) <- peekAdvance ptr
+    return $! TSNode p o1 o2 o3
+  poke ptr (TSNode p o1 o2 o3) = do
+    ptr <- pokeAdvance (castPtr ptr) p
+    ptr <- pokeAdvance ptr o1
+    ptr <- pokeAdvance ptr o2
+    ptr <- pokeAdvance ptr o3
+    return ()
+
 
 
 foreign import ccall "src/bridge.c ts_document_root_node_p" ts_document_root_node_p :: Ptr Document -> Ptr Node -> IO ()
