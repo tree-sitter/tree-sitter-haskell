d943099beff9fde1ea8d2977de81b1ab2c150a2b
Rob Rix <rob.rix@me.com>
2017-04-11 09:39:38 -0400

    Add foreign imports for ts_language_symbol_count & ts_language_symbol_name.

--- src/Text/Parser/TreeSitter/Language.hs
+++ src/Text/Parser/TreeSitter/Language.hs
@@ -1,5 +1,9 @@
 module Text.Parser.TreeSitter.Language where
 
+import Data.Word
+
 newtype Language = Language ()
   deriving (Show, Eq)
-  
+
+foreign import ccall unsafe "vendor/tree-sitter/include/tree_sitter/runtime.h ts_language_symbol_count" ts_language_symbol_count :: Ptr Language -> Word32
+foreign import ccall unsafe "vendor/tree-sitter/include/tree_sitter/runtime.h ts_language_symbol_name" ts_language_symbol_name :: Ptr Language -> Word16 -> CString
