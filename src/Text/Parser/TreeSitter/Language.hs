0fa4601c36df8a0724ff8a7d418c1040b4c3c584
Rob Rix <rob.rix@me.com>
2017-04-11 11:19:18 -0400

    Define a TH action to produce a datatype.

--- src/Text/Parser/TreeSitter/Language.hs
+++ src/Text/Parser/TreeSitter/Language.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Text.Parser.TreeSitter.Language where
 
 import Data.Word
 import Foreign.C.String
 import Foreign.Ptr
+import Language.Haskell.TH
 
 newtype Language = Language ()
   deriving (Show, Eq)
@@ -11,3 +13,12 @@ type TSSymbol = Word16
 
 foreign import ccall unsafe "vendor/tree-sitter/include/tree_sitter/runtime.h ts_language_symbol_count" ts_language_symbol_count :: Ptr Language -> Word32
 foreign import ccall unsafe "vendor/tree-sitter/include/tree_sitter/runtime.h ts_language_symbol_name" ts_language_symbol_name :: Ptr Language -> TSSymbol -> CString
+
+
+-- | TemplateHaskell construction of a datatype for the referenced Language.
+mkSymbolDatatype :: Name -> Ptr Language -> Q Dec
+mkSymbolDatatype name language = do
+  let symbolCount = ts_language_symbol_count language
+  symbolNames <- runIO $ traverse (peekCString . ts_language_symbol_name language) [0..fromIntegral (pred symbolCount)]
+
+  pure $! DataD [] name [] Nothing [  ] [ ConT (mkName "Show"), ConT (mkName "Eq"), ConT (mkName "Enum") ]
