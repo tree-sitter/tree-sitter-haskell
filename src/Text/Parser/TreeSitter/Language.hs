22a68e9c4a85601a44575e46d33df61b0d127913
Rob Rix <rob.rix@me.com>
2017-04-11 09:41:27 -0400

    Define a type synonym for the symbol parameter.

--- src/Text/Parser/TreeSitter/Language.hs
+++ src/Text/Parser/TreeSitter/Language.hs
@@ -5,5 +5,7 @@ import Data.Word
 newtype Language = Language ()
   deriving (Show, Eq)
 
+type TSSymbol = Word16
+
 foreign import ccall unsafe "vendor/tree-sitter/include/tree_sitter/runtime.h ts_language_symbol_count" ts_language_symbol_count :: Ptr Language -> Word32
-foreign import ccall unsafe "vendor/tree-sitter/include/tree_sitter/runtime.h ts_language_symbol_name" ts_language_symbol_name :: Ptr Language -> Word16 -> CString
+foreign import ccall unsafe "vendor/tree-sitter/include/tree_sitter/runtime.h ts_language_symbol_name" ts_language_symbol_name :: Ptr Language -> TSSymbol -> CString
