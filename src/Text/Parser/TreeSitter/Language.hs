708c0937c96c953c7f8903849d996ea3958e27d5
Rob Rix <rob.rix@me.com>
2017-04-11 11:19:51 -0400

    Add a function to convert a string to constructor case.

--- src/Text/Parser/TreeSitter/Language.hs
+++ src/Text/Parser/TreeSitter/Language.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Text.Parser.TreeSitter.Language where
 
+import Data.Char
 import Data.Word
 import Foreign.C.String
 import Foreign.Ptr
@@ -22,3 +23,11 @@ mkSymbolDatatype name language = do
   symbolNames <- runIO $ traverse (peekCString . ts_language_symbol_name language) [0..fromIntegral (pred symbolCount)]
 
   pure $! DataD [] name [] Nothing [  ] [ ConT (mkName "Show"), ConT (mkName "Eq"), ConT (mkName "Enum") ]
+
+toTitleCase :: String -> String
+toTitleCase s = case s of
+  c:rest -> toUpper c : go rest
+  _ -> go s
+  where go "" = ""
+        go ('_':c:rest) = toUpper c : go rest
+        go (c:rest) = c : go rest
