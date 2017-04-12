86438672f0c73ffdd1c89844d6266e19bcf94334
Rob Rix <rob.rix@me.com>
2017-04-12 10:04:54 -0400

    Gather symbol types as well as symbols.

--- src/Text/Parser/TreeSitter/Language.hs
+++ src/Text/Parser/TreeSitter/Language.hs
@@ -32,13 +32,12 @@ languageSymbols language = for [0..fromIntegral (pred count)] $ \ symbol -> do
 -- | TemplateHaskell construction of a datatype for the referenced Language.
 mkSymbolDatatype :: Name -> Ptr Language -> Q [Dec]
 mkSymbolDatatype name language = do
-  let symbolCount = ts_language_symbol_count language
-  symbolNames <- runIO $ traverse (peekCString . ts_language_symbol_name language) [0..fromIntegral (pred symbolCount)]
+  symbols <- runIO $ languageSymbols language
 
-  pure [ DataD [] name [] Nothing (flip NormalC [] . mkName . toTitleCase <$> symbolNames) [ ConT ''Show, ConT ''Eq, ConT ''Enum, ConT ''Ord ] ]
+  pure [ DataD [] name [] Nothing (flip NormalC [] . uncurry symbolToName <$> symbols) [ ConT ''Show, ConT ''Eq, ConT ''Enum, ConT ''Ord ] ]
 
-toTitleCase :: String -> String
-toTitleCase = (>>= initUpper) . map (>>= toDescription) . toWords
+symbolToName :: TSSymbolType -> String -> Name
+symbolToName ty = mkName . (>>= initUpper) . map (>>= toDescription) . toWords
   where toWords = split (condense (whenElt (not . isAlpha)))
         initUpper (c:cs) = toUpper c : cs
         initUpper "" = ""
