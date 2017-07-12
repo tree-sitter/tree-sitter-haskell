60c398eb69510ab2937c6ef1c97ee196526071dc
Rob Rix <rob.rix@me.com>
2017-07-12 14:43:26 -0400

    :fire: the derived Enum instance.

--- src/Text/Parser/TreeSitter/Language.hs
+++ src/Text/Parser/TreeSitter/Language.hs
@@ -37,7 +37,7 @@ mkSymbolDatatype name language = do
 
   Module _ modName <- thisModule
   pure
-    [ DataD [] name [] Nothing (flip NormalC [] . mkName <$> namedSymbols) [ ConT ''Show, ConT ''Eq, ConT ''Enum, ConT ''Ord, ConT ''Bounded, ConT ''Ix ]
+    [ DataD [] name [] Nothing (flip NormalC [] . mkName <$> namedSymbols) [ ConT ''Show, ConT ''Eq, ConT ''Ord, ConT ''Bounded, ConT ''Ix ]
     , InstanceD Nothing [] (AppT (ConT ''Symbol) (ConT name)) [ FunD 'symbolType (uncurry (clause modName) <$> symbols) ]
     , InstanceD Nothing [] (AppT (ConT ''Enum) (ConT name))
       [ FunD 'toEnum (zipWith (toEnumClause modName) [-1..] namedSymbols)
