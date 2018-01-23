e386d242253ab199ea34b4dc055e232f145f1f72
Rob Rix <rob.rix@me.com>
2018-01-23 15:59:04 -0500

    Remove the dependency on c-storable-deriving.

--- src/TreeSitter/Node.hs
+++ src/TreeSitter/Node.hs
@@ -5,7 +5,6 @@ module TreeSitter.Node where
 import Prelude
 import Foreign
 import Foreign.C
-import Foreign.CStorable
 import GHC.Generics
 import TreeSitter.Document
 
@@ -20,69 +19,72 @@ data Node = Node
   , nodeNamedChildCount :: !Int32
   , nodeChildCount :: !Int32
   }
-  deriving (Show, Eq, Generic, CStorable)
+  deriving (Show, Eq, Generic)
 
 data TSPoint = TSPoint { pointRow :: !Int32, pointColumn :: !Int32 }
-  deriving (Show, Eq, Generic, CStorable)
-
+  deriving (Show, Eq, Generic)
 
 data TSNode = TSNode !(Ptr ()) !Int32 !Int32 !Int32
-  deriving (Show, Eq, Generic, CStorable)
+  deriving (Show, Eq, Generic)
 
 peekAdvance :: forall a b. Storable a => Ptr a -> IO (a, Ptr b)
 peekAdvance ptr = do
   let aligned = alignPtr ptr (alignment (undefined :: a))
   a <- peek aligned
-  return (a, castPtr aligned `plusPtr` sizeOf a)
+  pure (a, castPtr aligned `plusPtr` sizeOf a)
 {-# INLINE peekAdvance #-}
 
 pokeAdvance :: forall a b. Storable a => Ptr a -> a -> IO (Ptr b)
 pokeAdvance ptr a = do
   let aligned = alignPtr ptr (alignment (undefined :: a))
   poke aligned a
-  return (castPtr aligned `plusPtr` sizeOf a)
+  pure (castPtr aligned `plusPtr` sizeOf a)
 {-# INLINE pokeAdvance #-}
 
 instance Storable Node where
   alignment _ = alignment (TSNode nullPtr 0 0 0 :: TSNode)
   sizeOf _ = 72
   peek ptr = do
-    (nodeTSNode, ptr) <- peekAdvance (castPtr ptr)
-    (nodeType, ptr) <- peekAdvance ptr
-    (nodeSymbol, ptr) <- peekAdvance ptr
-    (nodeStartPoint, ptr) <- peekAdvance ptr
-    (nodeEndPoint, ptr) <- peekAdvance ptr
-    (nodeStartByte, ptr) <- peekAdvance ptr
-    (nodeEndByte, ptr) <- peekAdvance ptr
-    (nodeNamedChildCount, ptr) <- peekAdvance ptr
-    (nodeChildCount, _) <- peekAdvance ptr
-    return $! Node nodeTSNode nodeType nodeSymbol nodeStartPoint nodeEndPoint nodeStartByte nodeEndByte nodeNamedChildCount nodeChildCount
-  poke = cPoke
+    (nodeTSNode,          ptr) <- peekAdvance (castPtr ptr)
+    (nodeType,            ptr) <- peekAdvance          ptr
+    (nodeSymbol,          ptr) <- peekAdvance          ptr
+    (nodeStartPoint,      ptr) <- peekAdvance          ptr
+    (nodeEndPoint,        ptr) <- peekAdvance          ptr
+    (nodeStartByte,       ptr) <- peekAdvance          ptr
+    (nodeEndByte,         ptr) <- peekAdvance          ptr
+    (nodeNamedChildCount, ptr) <- peekAdvance          ptr
+    (nodeChildCount, _)        <- peekAdvance          ptr
+    pure $! Node nodeTSNode nodeType nodeSymbol nodeStartPoint nodeEndPoint nodeStartByte nodeEndByte nodeNamedChildCount nodeChildCount
+  poke _ _ = pure ()
 
 instance Storable TSPoint where
   alignment _ = alignment (0 :: Int32)
   sizeOf _ = 8
   peek ptr = do
-    (pointRow, ptr) <- peekAdvance (castPtr ptr)
-    (pointColumn, _) <- peekAdvance ptr
-    return $! TSPoint pointRow pointColumn
-  poke = cPoke
+    (pointRow,    ptr) <- peekAdvance (castPtr ptr)
+    (pointColumn, _)   <- peekAdvance          ptr
+    pure $! TSPoint pointRow pointColumn
+  poke ptr (TSPoint r c) = do
+    ptr <- pokeAdvance (castPtr ptr) r
+    _   <- pokeAdvance          ptr  c
+    pure ()
+
 
 instance Storable TSNode where
   alignment _ = alignment (nullPtr :: Ptr ())
   sizeOf _ = 24
   peek ptr = do
-    (p, ptr) <- peekAdvance (castPtr ptr)
-    (o1, ptr) <- peekAdvance ptr
-    (o2, ptr) <- peekAdvance ptr
-    (o3, _) <- peekAdvance ptr
-    return $! TSNode p o1 o2 o3
+    (p,  ptr) <- peekAdvance (castPtr ptr)
+    (o1, ptr) <- peekAdvance          ptr
+    (o2, ptr) <- peekAdvance          ptr
+    (o3, _)   <- peekAdvance          ptr
+    pure $! TSNode p o1 o2 o3
   poke ptr (TSNode p o1 o2 o3) = do
     ptr <- pokeAdvance (castPtr ptr) p
-    ptr <- pokeAdvance ptr o1
-    ptr <- pokeAdvance ptr o2
-    _ <- pokeAdvance ptr o3
-    return ()
+    ptr <- pokeAdvance          ptr  o1
+    ptr <- pokeAdvance          ptr  o2
+    _   <- pokeAdvance          ptr  o3
+    pure ()
 
 
 
