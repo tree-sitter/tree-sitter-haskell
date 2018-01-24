55381329b3401dbf2c274ff3ba0eb81c597ddfe4
Rob Rix <rob.rix@me.com>
2018-01-24 14:24:21 -0500

    Test that Node roundtrips correctly.

--- test/Spec.hs
+++ test/Spec.hs
@@ -24,6 +24,9 @@ main = hspec $ do
     it "has the same size as its C counterpart" $
       sizeOf (undefined :: Node) `shouldBe` fromIntegral sizeof_node
 
+    it "roundtrips correctly" $
+      with (Node (TSNode nullPtr 1 2) nullPtr 1 (TSPoint 2 3) (TSPoint 4 5) 6 7 8 9) peek `shouldReturn` Node (TSNode nullPtr 1 2) nullPtr 1 (TSPoint 2 3) (TSPoint 4 5) 6 7 8 9
+
 foreign import ccall unsafe "src/bridge.c sizeof_tsnode" sizeof_tsnode :: CSize
 foreign import ccall unsafe "src/bridge.c sizeof_tspoint" sizeof_tspoint :: CSize
 foreign import ccall unsafe "src/bridge.c sizeof_node" sizeof_node :: CSize
