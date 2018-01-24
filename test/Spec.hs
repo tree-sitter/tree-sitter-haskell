997965ef63e5c7eba2107059b2558307b6e128c2
Rob Rix <rob.rix@me.com>
2018-01-24 12:36:26 -0500

    Test that TSPoint has the same size as its C counterpart.

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,4 +9,9 @@ main = hspec $ do
     it "should have the same size as its C counterpart" $ do
       sizeOf (undefined :: TSNode) `shouldBe` fromIntegral sizeof_tsnode
 
+  describe "TSPoint" $ do
+    it "should have the same size as its C counterpart" $ do
+      sizeOf (undefined :: TSPoint) `shouldBe` fromIntegral sizeof_tspoint
+
 foreign import ccall unsafe "src/bridge.c sizeof_tsnode" sizeof_tsnode :: CSize
+foreign import ccall unsafe "src/bridge.c sizeof_tspoint" sizeof_tspoint :: CSize
