707034e23263d78028982ef626f7187884eaaee8
Rob Rix <rob.rix@me.com>
2018-01-24 12:34:30 -0500

    Test that TSNode has the same size as its C counterpart.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,12 @@
+import Foreign.C.Types
+import Foreign.Storable
+import Test.Hspec
+import TreeSitter.Node
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "TSNode" $ do
+    it "should have the same size as its C counterpart" $ do
+      sizeOf (undefined :: TSNode) `shouldBe` fromIntegral sizeof_tsnode
+
+foreign import ccall unsafe "src/bridge.c sizeof_tsnode" sizeof_tsnode :: CSize
