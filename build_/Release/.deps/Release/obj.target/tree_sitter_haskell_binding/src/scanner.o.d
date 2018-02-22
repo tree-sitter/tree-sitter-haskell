4d082bee23ec40e2e2ee97e9f3e14363756b77af
clojj <jwin1968@gmail.com>
2018-02-22 22:26:22 +0100

    delete build_

--- build_/Release/.deps/Release/obj.target/tree_sitter_haskell_binding/src/scanner.o.d
+++
@@ -1,5 +0,0 @@
-cmd_Release/obj.target/tree_sitter_haskell_binding/src/scanner.o := g++ '-DNODE_GYP_MODULE_NAME=tree_sitter_haskell_binding' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/jwin/.electron-gyp/.node-gyp/iojs-1.7.11/include/node -I/home/jwin/.electron-gyp/.node-gyp/iojs-1.7.11/src -I/home/jwin/.electron-gyp/.node-gyp/iojs-1.7.11/deps/uv/include -I/home/jwin/.electron-gyp/.node-gyp/iojs-1.7.11/deps/v8/include -I../node_modules/nan -I../src  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/tree_sitter_haskell_binding/src/scanner.o.d.raw   -c -o Release/obj.target/tree_sitter_haskell_binding/src/scanner.o ../src/scanner.cc
-Release/obj.target/tree_sitter_haskell_binding/src/scanner.o: \
- ../src/scanner.cc ../src/tree_sitter/parser.h
-../src/scanner.cc:
-../src/tree_sitter/parser.h:
--- build_/Release/.deps/Release/tree_sitter_haskell_binding.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/tree_sitter_haskell_binding.node := rm -rf "Release/tree_sitter_haskell_binding.node" && cp -af "Release/obj.target/tree_sitter_haskell_binding.node" "Release/tree_sitter_haskell_binding.node"
