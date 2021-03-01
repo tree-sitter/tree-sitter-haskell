77fece386afdb0ae5e46cba5c0678f8fd707bac9
Torsten Schmits <torstenschmits@gmail.com>
2021-03-01 02:06:49 +0100

    refactor number literals

--- grammar/basic.js
+++ grammar/basic.js
@@ -1,34 +1,20 @@
 const
-  digit = /[0-9]/
-  digit_hex = /[0-9a-fA-F]/
-  digit_octal = /[0-7]/
-  decimals = repeat1(digit)
 
-  // TODO why not regex? is it legal to have whitespace inbetween?
-  hex = seq(choice('x', 'X'), repeat1(digit_hex))
-  octal = seq(choice('o', 'O'), repeat1(digit_octal))
-
-  literal_decimal = seq(digit, repeat(digit))
-  literal_hex = seq('0', hex)
-  literal_octal = seq('0', octal)
-
-  exponent = seq(
-    choice('e', 'E'),
-    optional(choice('+', '-')),
-    repeat1(literal_decimal)
-  )
-
-  literal_float = choice(
-    seq(decimals, '.', decimals, optional(exponent)),
-    seq(decimals, exponent)
-  )
+decimal = /[0-9][0-9_]*/
+exponent = /[eE][+-]?[0-9_]+/
 
 module.exports = {
   // ------------------------------------------------------------------------
   // literals
   // ------------------------------------------------------------------------
 
-  float: _ => token(literal_float),
+  float: _ => token(seq(
+    decimal,
+    choice(
+      seq(/\.[0-9_]+/, optional(exponent)), 
+      exponent,
+    ),
+  )),
 
   char: _ => /'([A-Za-z0-9();\[\]`ʹ{}_!#$%&⋆+,./<=>?@^" |\-~:\\*]|\\[a-zA-Z0-9\\"'&]*|\\\^[0-9A-Z@\[\]\\\^_])'/,
 
@@ -42,11 +28,13 @@ module.exports = {
     '"',
   )),
 
-  _integer_literal: _ => token(literal_decimal),
-  _octal_literal: _ => token(literal_octal),
-  _hex_literal: _ => token(literal_hex),
+  _integer_literal: _ => token(decimal),
+  _binary_literal: _ => token(/0[bB][01_]+/),
+  _octal_literal: _ => token(/0[oO][0-7]+/),
+  _hex_literal: _ => token(/0[xX][0-9a-fA-F_]+/),
 
   integer: $ => choice(
+    $._binary_literal,
     $._integer_literal,
     $._octal_literal,
     $._hex_literal,
