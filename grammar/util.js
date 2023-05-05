e89ec146413ee044b34904d2f3372cedc16a2383
Torsten Schmits <git@tryp.io>
2023-05-05 20:48:50 +0200

    allow multiple semicolons between statements and one leading semicolon

--- grammar/util.js
+++ grammar/util.js
@@ -20,7 +20,7 @@ sep2 = (sep, rule) => seq(rule, repeat1(seq(sep, rule)))
 
 /**
   * Wrap a repeated rule with semicolon rules.
-  * Between any two occurences of a rule in a layout, if no explicit semicolon is encountered, delegate to the scanner
+  * Between any two occurrences of a rule in a layout, if no explicit semicolon is encountered, delegate to the scanner
   * to determine heuristically where a statement or decl may end.
   * After the last repetition, the semicolon is optional.
   * The dynamic precision is needed because of some irregularities with standalone deriving decls and data deriving
@@ -31,7 +31,11 @@ terminated = ($, rule) => seq(
   optional(choice(';', $._layout_semicolon)),
 )
 
-layouted_braces = rule => braces(sep(';', rule), optional(';')),
+/**
+  * Explicitly braced layouts may have arbitrary numbers of semicolons before and after each statement, but this causes
+  * strange conflicts, so we only allow them once in leading and trailing position, but many times between statements.
+  */
+layouted_braces = rule => braces(optional(';'), sep(repeat1(';'), rule), optional(';')),
 
 /**
   * Wrap a repeated rule in a layout.
