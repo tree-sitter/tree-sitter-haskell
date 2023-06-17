a75238fdefc2281cdba7dc4dca13f1d68a91f177
Amaan Qureshi <amaanq12@gmail.com>
2023-06-17 06:07:12 -0400

    fix: free indents on destroy (#100)

--- src/scanner.c
+++ src/scanner.c
@@ -1663,4 +1663,5 @@ void tree_sitter_haskell_external_scanner_deserialize(void *indents_v, char *buf
 void tree_sitter_haskell_external_scanner_destroy(void *indents_v) {
   indent_vec *indents = (indent_vec*) indents_v;
   VEC_FREE(indents);
+  free(indents);
 }
